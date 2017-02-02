0f2ee8ade3bca71c7fc7757b74aed4a4d8ef3070
Jonas Felix <jonas.felix@felixideas.ch>
2017-02-02 08:43:43 +0100

    deploy static slides

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
 git checkout gh-pages
+reveal-md `ls -1t *.md | head -n 1` --theme letsboot-sky --highlightTheme github --static > index.html
+sed -i -e 's;<head>;<head><base href="https://wingsuitist.github.io/angular-basel-meetup/">;g' index.html
+git add -A
+git commit -m 'static deployment'
+git push origin gh-pages
+git checkout master
