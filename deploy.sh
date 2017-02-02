712db62139fe4db88db5cf2f6d8bf80a392167e0
Jonas Felix <jonas.felix@felixideas.ch>
2017-02-02 09:19:24 +0100

    static deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git checkout gh-pages && \
   git checkout master -- . && \
   reveal-md `ls -1t *.md | head -n 1` --theme letsboot-sky --highlightTheme github --static > index.html && \
-  sed -i -e 's;<head>;<head><base href="https://wingsuitist.github.io/angular-basel-meetup/">;g' index.html && /bin/rm "index.html-e" && \
+  sed -i -e 's;<head>;<head><base href="https://wingsuitist.github.io/angular-basel-meetup/">;g' index.html && rm "index.html-e" && \
   git add -A && \
   git commit -m 'static deployment' && \
   git push origin gh-pages && \
