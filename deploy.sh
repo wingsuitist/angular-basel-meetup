b95658cd171d5ac55dd4185f0c4975f35bb2a5ad
Jonas Felix <jonas.felix@felixideas.ch>
2017-06-28 18:02:30 +0200

    use newest slide for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git checkout gh-pages && \
   git checkout master -- . && \
-  reveal-md `ls -1t *.md | head -n 1` --theme letsboot-sky --highlightTheme github --static > index.html && \
+  reveal-md `ls -r1 *-*.md | head -n 1` --theme letsboot-sky --highlightTheme github --static > index.html && \
   sed -i -e 's;<head>;<head><base href="https://wingsuitist.github.io/angular-basel-meetup/">;g' index.html && rm "index.html-e" && \
   git add -A && \
   git commit -m 'static deployment' && \
