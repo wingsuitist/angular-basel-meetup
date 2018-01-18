38eb6f0e100ca09fbadd4b0468a4a08eab8620e5
Stefani Gerber <stefani.gerber@bluesky-it.ch>
2018-01-18 14:58:15 +0100

    content for 2018-01-17 and added logo

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Angular Basel Meetup Slides
 
 Usage:
+npm install -g reveal-md
 sh present.sh
 
 [View slides](https://wingsuitist.github.io/angular-basel-meetup/index.html)
---
+++ develop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# picks newest (i.e. last edited) md-file and displays it using reveal-md
+
+reveal-md `ls -1t *.md | head -n 1` --theme ../theme/letsboot-sky.css --highlightTheme github --title='Angular Meetup Basel' --watch
