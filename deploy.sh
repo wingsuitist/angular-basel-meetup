a6f48adfb88b1df66daa35e44580b781e4ff89cc
Jonas Felix <jonas.felix@felixideas.ch>
2018-03-03 10:06:00 +0100

    adapted to new reveal-md

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git checkout gh-pages && \
   git checkout master -- . && \
-  reveal-md `ls -r1 *-*.md | head -n 1` --theme theme/letsboot-sky.css --highlightTheme github --static ./ > index.html && \
+  reveal-md `ls -r1 *-*.md | head -n 1` --theme _assets/theme/letsboot-sky.css --highlightTheme github --static ./  && \
   sed -i -e 's;<head>;<head><base href="https://wingsuitist.github.io/angular-basel-meetup/">;g' index.html && rm "index.html-e" && \
   git add -A && \
   git commit -m 'static deployment' && \
